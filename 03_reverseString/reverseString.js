75ebf09c5aeb24bd4102f61a9a2f1edd53c3eb8a
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-08 19:58:01 +0300

    Finished 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let splitText = text.split("");
+    let returnText = ``;
+    for (let i = splitText.length-1; i > -1; i--){
+        returnText = `${returnText}${splitText[i]}`
+    }
+    return returnText;
 };
 
 // Do not edit below this line
