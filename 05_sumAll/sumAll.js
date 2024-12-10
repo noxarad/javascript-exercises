f09aa45ee4454e2f21a51fe552ee50a15a32c2e2
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-10 20:03:41 +0300

    Finished 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return "ERROR"
+    }
+    if (num1 !== parseInt(num1) || num2 !== parseInt(num2)){
+        return "ERROR"
+    }
+    if (typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR"
+    }
+    let low
+    let high
+    let sum = 0
+    if (num1 < num2){
+        high = num2
+        low = num1
+    } else {
+        high = num1
+        low = num2
+    }
+    for (let i = low; i <= high; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
