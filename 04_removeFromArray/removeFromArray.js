ac89365a5e874081a91fd0e2488bd78be0146d86
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-10 18:12:35 +0300

    Finished 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToBeRemoved) {
+    for (const item of itemsToBeRemoved){
+        const index = array.indexOf(item)
+        if (index !== -1){
+            array.splice(index,1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
