882622c3df7cef54bcf8d21ff59109d6baddf9a5
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-10 21:02:50 +0300

    Finished 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemp = (temperature - 32 ) * (5/9)
+  return Math.round(convertedTemp * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemp = temperature * (9/5) + 32
+  return Math.round(convertedTemp * 10) / 10
 };
 
 // Do not edit below this line
