741c5aa6007499435bf5c86dee6fce444e596c49
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-14 10:07:39 +0300

    Finished 10 exercise Also at some point I changed the palindromes test file or didn't commit it last time, this'll hopefully be my last blunder

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    place = Number(place)
+    if (place < 0){
+        return "OOPS"
+    }
+    if (place === 1 || place === 2){
+        return 1
+    } else if (place === 0){
+        return 0
+    }
+    return fibonacci(place - 1) + fibonacci(place - 2)
 };
 
 // Do not edit below this line
