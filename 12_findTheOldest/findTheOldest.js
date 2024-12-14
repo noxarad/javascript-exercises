2f71d9de7d0a32c22980df8dd8a2d2153e56cd0f
noxar_ad <71936957+noxarad@users.noreply.github.com>
2024-12-14 20:41:37 +0300

    Finished 12 exercise Yay :)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(listOfPeople) {
+    let oldestPerson = listOfPeople[0]
+    if (oldestPerson.yearOfDeath === undefined){
+        oldestPerson.yearOfDeath = 2024
+    }
+    for (person of listOfPeople){
+        if (person.yearOfDeath === undefined){
+            person.yearOfDeath = 2024
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth){
+            oldestPerson = person
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
