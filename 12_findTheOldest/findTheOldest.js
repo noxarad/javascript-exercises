0823ff56b0c791ff9ad9f6e942729794ab5af415
noxar_ad <71936957+noxarad@users.noreply.github.com>
2025-03-03 17:56:47 +0300

    Fixed answer 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    peoople = people.map((person) => {
+        if (person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+    })
+    return people.sort((person1, person2) => (person2.yearOfDeath - person2.yearOfBirth) - (person1.yearOfDeath - person1.yearOfBirth))[0]
 };
 
 // Do not edit below this line
