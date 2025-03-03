d627a0e721b11189dfed8982fde3e51665a698f2
noxar_ad <71936957+noxarad@users.noreply.github.com>
2025-03-03 06:33:44 +0300

    Fixed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(...nums) {
+  return nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, ...nums) {
+	return nums.reduce((accumulator, currentValue) => accumulator - currentValue, num1)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let originNum = num
+  num = 1
+  for (let i = 0; i < power; i++){
+    num = num * originNum
+  }
+  return num
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1
+  } else if (num -1 > 0){
+    num *= factorial(num - 1)
+  }
+  return num
 };
 
 // Do not edit below this line
