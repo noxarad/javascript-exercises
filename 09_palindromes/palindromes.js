8de324d358b3884650b88737fcf0a7c5a0ad2872
noxar_ad <71936957+noxarad@users.noreply.github.com>
2025-03-03 16:17:21 +0300

    Fixed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = palindrome.toUpperCase()
+   palindrome = palindrome.split("")
+    let regEx = /^[a-z0-9]+$/i;
+   palindrome = palindrome.filter((letter) => regEx.test(letter))
+    console.log(palindrome)
+    let j = palindrome.length - 1
+    let i = 0
+    while (i < j) {
+        if (palindrome[i] === palindrome[j]) {
+            i++
+            j--
+        } else {
+            return false
+            break
+        }
+    }
+    return true
 };
-
 // Do not edit below this line
 module.exports = palindromes;
